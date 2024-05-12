636684ba93992a08e1b05436b2b56a0a31d7a3d6
RayenRk <rakkadrayen@gmail.com>
2024-05-12 23:33:38 +0100

    show user email after login

--- app/src/main/java/com/example/classroom/MainActivity.java
+++ app/src/main/java/com/example/classroom/MainActivity.java
@@ -3,21 +3,33 @@ package com.example.classroom;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     MainAdapter adapter;
-
     FloatingActionButton actionBtn;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String email;
+    TextView userEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.rv);
         actionBtn = findViewById(R.id.fab);
+        userEmail = findViewById(R.id.user);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // get the email of the user who just logged in from Login Activity from Firebase and set it to the TextView
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        email = fAuth.getCurrentUser().getEmail().toString();
+        userEmail.setText(email);
+
 
         FirebaseRecyclerOptions<UserModel> options =
                 new FirebaseRecyclerOptions.Builder<UserModel>()
@@ -38,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, AddActivity.class));
+                finish();
             }
         });
 
